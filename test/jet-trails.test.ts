2f430c78dfac69a289e90546a518d4d0eea48d28
Ivan <Yalovechik2012@gmail.com>
2024-04-28 22:00:46 +0300

    Initial commit

---
+++ test/jet-trails.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JetTrails from '../lib/jet-trails-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jet-trails-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JetTrails.JetTrailsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
